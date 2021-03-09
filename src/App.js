12670c15d700f44474885379a866db8c7a92e840
Jin Won Lee <0125jinwonlee@gmail.com>
2021-03-08 22:35:59 -0800

    New Portfolio in Progress

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Container">
+        <header className="Header">
+          <div className="HeaderText">
+            <h1>JIN WON LEE</h1>
+            <h2>Front-End Web Developer</h2>
+          </div>
+          <div className="HeaderNav">
+            <a href="/">Contact</a>
+            <a href="/">Resume</a>
+          </div>
+        </header>
+        <main className="Main">
+          <div className="MainText">
+            <h1>
+              HELLO, I AM JIN.
+              <br />
+              I HAVE A PASSION FOR CODING,
+              <br />
+              AND SOCCER!
+            </h1>
+            <p>
+              As I found my passion for coding throughout my career, I have been
+              teaching myself JavaScript, React, HTML, and CSS focusing on
+              front-end web development.
+              <br />
+              Under the idea to have as much practical experience as possible, I
+              built my personal project called “Better World” and I also have
+              been working on small JavaScript projects to expand my coding
+              experience.
+            </p>
+          </div>
+          <div className="MainImage">
+            <img src="jin.jpeg" alt="Jin" className="JinImage" />
+          </div>
+        </main>
+        <footer className="Footer"></footer>
+      </div>
     </div>
   );
 }
